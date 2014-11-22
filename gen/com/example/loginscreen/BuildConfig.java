26339b96c7a880124b8ca08e2f5970af14d04ae2
Rossem <rrsbrg@gmail.com>
2014-11-22 00:30:26 -0500

    first commit

--- gen/com/example/loginscreen/BuildConfig.java
+++ gen/com/example/loginscreen/BuildConfig.java
@@ -1,5 +1,5 @@
 /** Automatically generated file. DO NOT MODIFY */
-package com.example.woahpaper;
+package com.example.loginscreen;
 
 public final class BuildConfig {
     public final static boolean DEBUG = true;
--- src/com/example/woahpaper/MainActivity.java
+++ src/com/example/woahpaper/MainActivity.java
@@ -1,36 +1,56 @@
 package com.example.woahpaper;
 
+
+
+import com.example.loginscreen.R;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
-import android.view.MenuItem;
-
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+//DOG
 public class MainActivity extends Activity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+   private EditText  username=null;
+   private EditText  password=null;
+   private Button login;
+   int counter = 3;
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_main);
+      username = (EditText)findViewById(R.id.editText1);
+      password = (EditText)findViewById(R.id.editText2);
+      login = (Button)findViewById(R.id.button1);
+   }
+
+   public void login(View view){
+      if(username.getText().toString().equals("ADMIN") && 
+      password.getText().toString().equals("6969")){
+      Toast.makeText(getApplicationContext(), "Redirecting...", 
+      Toast.LENGTH_SHORT).show();}
+   }	
+   /*else{
+      Toast.makeText(getApplicationContext(), "Wrong Credentials",
+      Toast.LENGTH_SHORT).show();
+      attempts.setBackgroundColor(Color.RED);	
+      counter--;
+      attempts.setText(Integer.toString(counter));
+      if(counter==0){
+         login.setEnabled(false);
+      }
+
+   }*/
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+   @Override
+   public boolean onCreateOptionsMenu(Menu menu) {
+      // Inflate the menu; this adds items to the action bar if it is present.
+      getMenuInflater().inflate(R.menu.main, menu);
+      return true;
+   }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 }
