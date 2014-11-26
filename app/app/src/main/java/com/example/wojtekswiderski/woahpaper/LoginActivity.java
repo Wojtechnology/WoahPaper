f9e741da0e281321a895f5e19a1b362fccf1aece
Wojtechnology <wswiders@uwaterloo.ca>
2014-11-26 01:39:38 -0500

    Added Filters and Other Stuff First Page Done Need to Implement GCM, HTTP Calls, and Second Page Perhaps Settings

--- app/app/src/main/java/com/example/wojtekswiderski/woahpaper/LoginActivity.java
+++ app/app/src/main/java/com/example/wojtekswiderski/woahpaper/LoginActivity.java
@@ -6,10 +6,18 @@ import android.os.Bundle;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.text.method.PasswordTransformationMethod;
+import android.util.Log;
 import android.view.Gravity;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.CompletionInfo;
+import android.view.inputmethod.CorrectionInfo;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.ExtractedText;
+import android.view.inputmethod.ExtractedTextRequest;
+import android.view.inputmethod.InputConnection;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -24,39 +32,33 @@ public class LoginActivity extends Activity {
         super.onCreate(extra);
         setContentView(R.layout.activity_login);
 
-        /*ScrollView scroller = new ScrollView(this);
+        EditText userInput = (EditText) findViewById(R.id.userBox);
+        EditText passInput = (EditText) findViewById(R.id.passBox);
 
-        LinearLayout layout = new LinearLayout(this);
-        layout.setOrientation(LinearLayout.VERTICAL);
-        layout.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT));
+        //Caps the input
+        InputFilter[] userFilterArray = new InputFilter[2];
+        userFilterArray[0] = new InputFilter.AllCaps();
+        userFilterArray[1] = new InputFilter.LengthFilter(12);
+        userInput.setFilters(userFilterArray);
 
-        //Creating objects to use as spaces in between tiles
-        int spaceCounter = 0;
-        TextView[] spaces = new TextView[4];
-        for(int i = 0; i < 4; i++){
-            spaces[i] = new TextView(this);
-            spaces[i].setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 10));
-        }
-        spaces[0].setBackgroundColor(0xFFFF8800);
-        spaces[1].setBackgroundColor(0xFFCC0000);
-        spaces[2].setBackgroundColor(0xFFCC0000);
-        spaces[3].setBackgroundColor(0xFF0099CC);
+        userInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if (((EditText) view).getHint().equals(""))
+                    ((EditText) view).setHint(R.string.username_hint);
+                else ((EditText) view).setHint("");
+            }
+        });
 
-        //Title for the app
-        final TextView titleView = new TextView(this);
-        titleView.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 200));
-        titleView.setBackgroundColor(0xFFFFBB33);
-        titleView.setTypeface(Typeface.create("", Typeface.BOLD));
-        titleView.setTextColor(0xFFFFFFFF);
-        titleView.setText("WOAHPAPER");
-        titleView.setGravity(Gravity.CENTER);
-        titleView.setTextSize(50f);
-        titleView.setPadding(0, 0, 0, -10);
-        layout.addView(titleView);
+        passInput.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean b) {
+                if(((EditText) view).getHint().equals("")) ((EditText) view).setHint(R.string.password_hint);
+                else ((EditText) view).setHint("");
+            }
+        });
 
-        layout.addView(spaces[spaceCounter++]);
-
-        //Filters for the inputs allowing usernames up to 12 letters
+        /*//Filters for the inputs allowing usernames up to 12 letters
         //Also caps the input
         InputFilter[] userFilterArray = new InputFilter[2];
         userFilterArray[0] = new InputFilter.LengthFilter(12);
@@ -80,8 +82,6 @@ public class LoginActivity extends Activity {
         userName.setTextColor(0xFFFFFFFF);
         layout.addView(userName);
 
-        layout.addView(spaces[spaceCounter++]);
-
         //password field
         final EditText passWord = new EditText(this);
         passWord.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 200));
@@ -96,27 +96,7 @@ public class LoginActivity extends Activity {
         passWord.setTextSize(50f);
         passWord.setCursorVisible(false);
         passWord.setTextColor(0xFFFFFFFF);
-        layout.addView(passWord);
-
-        layout.addView(spaces[spaceCounter++]);
-
-        //Submit button for the app
-        final Button submit = new Button(this);
-        submit.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 200));
-        submit.setBackgroundColor(0xFF33B5E5);
-        submit.setTextSize(50f);
-        submit.setTextColor(0xFFFFFFFF);
-        submit.setTypeface(Typeface.create("", Typeface.BOLD));
-        submit.setText("SIGN IN");
-        layout.addView(submit);
-
-        layout.addView(spaces[spaceCounter++]);
-
-        setContentView(scroller);
-        scroller.addView(layout);
-
-        View root = layout.getRootView();
-        root.setBackgroundColor(0xFF99CC00);*/
+        layout.addView(passWord);*/
 
     }
 
