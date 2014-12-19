3791d41310dd868fadb3365757e5e0326314c8ce
Wojtechnology <wswiders@uwaterloo.ca>
2014-12-18 21:30:07 -0500

    Major changes to server/Using UUID

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ users.once('open', function callback () {
 
 //Create user model
 var userSchema = mongoose.Schema({
+	uuid : String,
 	username : String,
-	password : Number
+	regid : String
 })
 
 var user = mongoose.model('user', userSchema);
@@ -34,34 +35,134 @@ app.listen( port, function() {
 	console.log( 'Express server listening on port %d in %s mode', port, app.settings.env );
 });
 
-app.get('/user/:username/:password', function(req, res){
+app.get('/new/:uuid/:username', function(req, res){
 	var me;
 	res.setHeader('Content-Type', 'application/json');
-	user.findOne({ 'username' : req.params.username }, function(err, userFound){
-		me = userFound;
-		if(!me){
-			me = new user({username : req.params.username, password : Number(req.params.password)});
-			me.save(function (err, me) {
-				if (err) return console.error(err);
-				res.end("created user");
-			});
+	user.findOne({'uuid' : req.params.uuid}, function(err, userFound){
+		if(err){
+			res.end("failure");
+			return console.error(err);
+		}
+		if(userFound){
+			res.end("uuid taken");
 		}else{
-			if(me.password === Number(req.params.password)) res.end("correct password");
-			else res.end("incorrect password");
+			user.findOne({'username' : req.params.username}, function(err1, userFound1){
+				if(err1){
+					res.end("failure");
+					return console.error(err1);
+				}
+				if(userFound1){
+					res.end("username taken");
+				}else{
+					me = new user({uuid : req.params.uuid, username : req.params.username, regid : ""});
+					me.save(function (err2, me) {
+						if (err2){
+							res.end("failure");
+							return console.error(err2);
+						}
+						res.end("created user");
+					});
+				}
+			});
+		}
+	});	
+});
+
+app.get('/login/:uuid', function(req, res){
+	var me;
+	res.setHeader('Content-Type', 'application/json');
+	user.findOne({'uuid' : req.params.uuid}, function(err, userFound){
+		me = userFound;
+		if(err){
+			res.end("failure");
+			return console.error(err);
+		}
+		if(!me){
+			res.end("no account");
+		}else{
+			res.end(me.username);
 		}
 	});
-	
+});
+
+app.get('/updateUser/:uuid/:username', function(req, res){
+	var me;
+	res.setHeader('Content-Type', 'application/json');
+	user.findOne({'username' : req.params.username}, function(err, userFound){
+		if(userFound){
+			res.end("user taken");
+		}else{
+			user.findOne({'uuid' : req.params.uuid}, function(err1, userFound1){
+				if(err1){
+					res.end("failure");
+					return console.error(err1);
+				}	
+				me = userFound1;
+				if(!me){
+					res.end("no user");
+				}else{
+					me.username = req.params.username;
+					me.save(function(err2, me){
+						if (err2){
+							res.end("failure");
+							return console.error(err2);
+						}
+						res.end("success");
+					});
+				}
+		
+			});
+		}
+	});
+});
+
+app.get('/updateReg/:uuid/:regid', function(req, res){
+	var me;
+	res.setHeader('Content-Type', 'application/json');
+	user.findOne({'regid' : req.params.regid}, function(err, userFound){
+		if(err){
+			res.end("failure");
+			return console.error(err);
+		}
+		if(userFound){
+			res.end("regid taken");
+		}else{
+			user.findOne({'uuid' : req.params.uuid}, function(err1, userFound1){
+				if(err1){
+					res.end("failure");
+					return console.error(err1);
+				}
+				me = userFound1;
+				if(!me){
+					res.end("no user");
+				}else{
+					me.regid = req.params.regid;
+					me.save(function(err2, me){
+						if(err2){
+							res.end("failure");
+							return console.error(err2);
+						}
+						res.end("success");
+					});
+				}
+			});
+		}
+	});
 });
 
 app.get('/send/:username/:word', function(req, res){
-	var you;
+	var me;
 	res.setHeader('Content-Type', 'application/json');
-	user.findOne({ 'username' : req.params.username }, function(err, userFound){
+	user.findOne({'username' : req.params.username}, function(err, userFound){
+		if(err){
+			res.end("failure");
+			return console.error(err);
+		}
 		me = userFound;
-		if(!me){
-			res.end("User not found")
+		if(!me || me.regid === ""){
+			res.end("no user")
 		}else{
-			res.end("Sent word " + req.params.word);
+			res.end("success");
 		}
 	});
 });
